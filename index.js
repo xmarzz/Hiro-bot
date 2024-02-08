98dff5e4e8722058c0b143cc7c1504c4869a6d4e
Marz <sathickjaffer44@gmail.com>
2024-02-08 19:40:35 +0530

    new bot

---
+++ index.js
@@ -0,0 +1,10 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/webhook',(req,res) => {
+    const 
+})
+
